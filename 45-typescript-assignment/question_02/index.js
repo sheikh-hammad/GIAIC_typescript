b3b4ebedd584f2db7a9ce103c2f5d60ffefe8a83
sheikh-hammad <hammadusman001@gmail.com>
2024-06-11 23:20:10 +0500

    publish q2

---
+++ 45-typescript-assignment/question_02/index.js
@@ -0,0 +1,9 @@
+"use strict";
+/*
+Personal Message: Store a person’s name in a variable, and print a
+message to that person. Your message should be simple, such as,
+“Hello Eric, would you like to learn some Python today?”
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+const personName = "Hammad";
+console.log(`"Hi ${personName}, would you like to learn some python today?`);
